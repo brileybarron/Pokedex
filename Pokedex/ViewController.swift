31f3fc3009ea6eab51520eaa88bd5813356dceda
Briley Barron <brileybarron@MacBook-Pro-101.local>
2018-11-12 10:20:07 -0600

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Briley Barron on 11/12/18.
+//  Copyright © 2018 Briley Barron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
