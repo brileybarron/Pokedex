347e3d84f1a67dc31f4be1132dad9c7e31db3c88
Briley Barron <brileybarron@MacBook-Pro-101.local>
2018-11-12 13:05:18 -0600

    sent info to api

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -11,7 +11,41 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController {
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var outputTextView: UITextView!
+    var pokedexAPIbaseURL = "https://pokeapi.co/api/v2/pokemon/"
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        //checking to see if the values are there
+        guard let pokemonIndex = input.text else {
+                return
+        }
+        
+        //clear out the text field
+        input.text = ""
 
+        
+        //Replacing all spaces with a "+" for the URL
+        let pokemonURLComponent = pokemonIndex.replacingOccurrences(of: " ", with: "+")
+
+        //building the complete request URL
+        let requestURL = pokedexAPIbaseURL + pokemonURLComponent
+        
+        Alamofire.request(requestURL).responseJSON { (response) in
+            switch response.result{
+            case .success(let value):
+                let json = JSON(value)
+                self.outputTextView.text = json["forms"]["name"].stringValue
+            case .failure(let error):
+                self.outputTextView.text = "Invalid selection entered or an error occured.  Please try Again!"
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
